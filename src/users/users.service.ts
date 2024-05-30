df710a8aa2278aa4282485d7c2304cd4f956ba52
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-05-30 18:07:36 -0300

    Added update, remove, and findOne methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -13,11 +13,38 @@ export class UsersService {
         return this.repo.save(user);
     }
 
-    findOne(id: number) {
-        return this.repo.findOneBy({ id });
+    async findOne(id: number) {
+        const myUser = await this.repo.findOneBy({ id });
+
+        if (!myUser) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+
+        return myUser;
     }
 
     find(email: string) {
         return this.repo.find({ where: { email } });
+    }
+
+    async update(id: number, userNewAttributes: Partial<User>) {
+        const myUser = await this.findOne(id);
+
+        if (!myUser) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+
+        Object.assign(myUser, userNewAttributes);
+        return this.repo.save(myUser);
+    }
+
+    async remove(id: number) {
+        const myUser = await this.findOne(id);
+
+        if (!myUser) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+
+        return this.repo.remove(myUser);
     }
 }

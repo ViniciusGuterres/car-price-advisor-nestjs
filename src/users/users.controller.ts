29667ae0bcca34df3e5eb5bcfa80022a461b1271
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-06-03 11:24:21 -0300

    Added ClassSerializerInterceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -8,6 +8,7 @@ export class UsersController {
     constructor(private usersService: UsersService) { }
 
     @Get('/:id')
+    @UseInterceptors(ClassSerializerInterceptor)
     async findUser(@Param('id') id: string) {
         const myUser = await this.usersService.findOne(parseInt(id));
 
@@ -19,6 +20,7 @@ export class UsersController {
     }
 
     @Get()
+    @UseInterceptors(ClassSerializerInterceptor)
     async findAllUsers(@Query('email') email: string) {
         const myUser = await this.usersService.find(email);
 

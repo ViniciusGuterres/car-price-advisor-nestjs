e3d8f47192793e08f70f4ec9ce0556d95fb93773
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-07-02 23:16:59 -0300

    Adjusted authService test module

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,26 +1,34 @@
 import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
+import { User } from "./user.entity";
 
-it('can create an instance of auth service', async () => {
-    // Creating a fake copy of users service
-    const fakeUsersService = {
-        find: () => Promise.resolve([]),
-        create: (email: string, password: string) => Promise.resolve({ id: 1, email, password }),
-    }
+describe('AuthService', () => {
+    let service: AuthService;
 
-    const module = await Test.createTestingModule({
-        providers: [
-            AuthService,
-            {
-                // If anyone ask for "UsersService" inside the AuthService, give them the "fakeUserService" object
-                provide: UsersService,
-                useValue: fakeUsersService
-            },
-        ],
-    }).compile();
+    beforeEach(async () => {
+        // Creating a fake copy of users service
+        const fakeUsersService: Partial<UsersService> = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string) =>
+                Promise.resolve({ id: 1, email, password } as User),
+        }
 
-    const service = module.get(AuthService);
+        const module = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    // If anyone ask for "UsersService" inside the AuthService, give them the "fakeUserService" object
+                    provide: UsersService,
+                    useValue: fakeUsersService
+                },
+            ],
+        }).compile();
 
-    expect(service).toBeDefined();
+        service = module.get(AuthService);
+    });
+
+    it('can create an instance of auth service', async () => {
+        expect(service).toBeDefined();
+    });
 });

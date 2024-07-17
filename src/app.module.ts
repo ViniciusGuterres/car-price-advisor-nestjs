f055a16c1c0008c33a82d02107060a89a089fa07
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-07-16 21:42:05 -0300

    Configurated nestjs config. Retrieved the configs from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,26 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: "postgres",
-    host: "myHost",
-    port: 5432,
-    username: "your-master-username",
-    password: "your-password",
-    database: "your-database-name",
-    entities: [User, Report],
-    synchronize: true
-  }), UsersModule, ReportsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report],
+        }
+      }
+    }), UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

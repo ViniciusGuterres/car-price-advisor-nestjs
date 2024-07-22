561cba5634ef53cd6a6c9b546d2cdf0a42ca61fd
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2024-07-22 12:20:00 -0300

    Formatted the code

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -6,7 +6,7 @@ import { User } from './user.entity';
 @Injectable()
 export class UsersService {
 
-    constructor(@InjectRepository(User) private repo: Repository<User>) {}
+    constructor(@InjectRepository(User) private repo: Repository<User>) { }
 
     create(email: string, password: string) {
         const user = this.repo.create({ email, password });
@@ -18,12 +18,12 @@ export class UsersService {
 
         let myUser = await this.repo.findOneBy({ id });
 
-        if (!myUser)  myUser = null;
-        
+        if (!myUser) myUser = null;
+
         return myUser;
     }
 
-    async find(email: string): Promise<Array<User>> {        
+    async find(email: string): Promise<Array<User>> {
         return await this.repo.find({ where: { email } });
     }
 
